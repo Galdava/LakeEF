9e99e1ec2ae3532151a73d7e8a39be971e3daa63
galdavaa <50417184+galdavaa@users.noreply.github.com>
2020-08-08 12:47:27 +0400

    Add project files.

---
+++ LakesEF/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+using System;
+using System.Data.Entity;
+using System.Linq;
+namespace LakesEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var data = new LakesContext())
+            {
+                var lakes = from l in data.Lakes
+                            where l.Country == "USA"
+                            select l;
+                foreach (Lakes l in lakes)
+                {
+                    Console.WriteLine(l.Name);
+                }
+            }
+        }
+    }
+
+    public partial class LakesContext : DbContext
+    {
+        public LakesContext() : base("name=LakesContext")
+        {
+
+        }
+        public DbSet<Lakes> Lakes { get; set; }
+     
+    }
+    public partial class Lakes
+    {
+        public int LakeId { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public decimal Area { get; set; }
+    }
+}
